74b2294d074009dfe66d1ac6621a849a1e5f91f0
Ristea Stefan <stefanristea1@yahoo.com>
2019-05-05 06:32:51 +0300

    Implemented Models

---
+++ WADFinal/WADFinal/Models/Developer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WADFinal.Models
+{
+    public class Developer : Account
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Game> Games { get; set; }
+    }
+}
---
+++ WADFinal/WADFinal/Models/Reviewer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WADFinal.Models
+{
+    public class Reviewer : Account
+    {
+        public int ReviewerId { get; set; }
+        public virtual ICollection<Game> ReviewedGames { get; set; }
+    }
+}
