25f05ec574ee42ed0bf960cd45dd4097bedcd93c
Ristea Stefan <stefanristea1@yahoo.com>
2019-05-05 07:53:37 +0300

    Created controllers and views and fixed errors in past commit

---
+++ WADFinal/WADFinal/Controllers/DeveloperController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WADFinal.DAL;
+using WADFinal.Models;
+
+namespace WADFinal.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        private SearchAppContext db = new SearchAppContext();
+
+        // GET: Developer
+        public ActionResult Index()
+        {
+            return View(db.Developers.ToList());
+        }
+
+        // GET: Developer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Developer developer = db.Developers.Find(id);
+            if (developer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(developer);
+        }
+
+        // GET: Developer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Developer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,User,Password,LoggedIn")] Developer developer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Developers.Add(developer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(developer);
+        }
+
+        // GET: Developer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Developer developer = db.Developers.Find(id);
+            if (developer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(developer);
+        }
+
+        // POST: Developer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,User,Password,LoggedIn")] Developer developer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(developer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(developer);
+        }
+
+        // GET: Developer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Developer developer = db.Developers.Find(id);
+            if (developer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(developer);
+        }
+
+        // POST: Developer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Developer developer = db.Developers.Find(id);
+            db.Developers.Remove(developer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
