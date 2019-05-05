128f3a9529a8e4b9fb5c4f4046d93e76007213e6
Ristea Stefan <stefanristea1@yahoo.com>
2019-05-05 07:03:43 +0300

    Implemented Context and Initializer

---
+++ WADFinal/WADFinal/DAL/SearchAppContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading.Tasks;
+using WADFinal.Models;
+
+namespace WADFinal.DAL
+{
+    public class SearchAppContext : DbContext
+    {
+        public SearchAppContext() : base("SearchAppContext")
+        {
+        }
+
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
